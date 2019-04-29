2e72a90abd076a74d8f45ac3695633467a48fa85
dahmri <dahmrik@gmail.com>
2019-04-29 18:10:40 +0200

    Add files via upload

---
+++ NetworkFeatures/Preferential_Attachment.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def preferential_attachment_score(graph):
+
+    A = graph.get_adjacency();
+    i_degree = np.zeros(A.shape[0])
+    for i in range(A.shape[0]):
+        i_degree[i] = len(graph.neighbors(i))
+        # If we want outdegree instead of total degree:
+        #i_degree[i] = graph.vs[i].outdegree()
+    PA = np.zeros(A.shape)
+    for i in range(PA.shape[0]):
+        for j in range(PA.shape[0]):
+            PA[i,j] = i_degree[i]*i_degree[j]
+    return PA
