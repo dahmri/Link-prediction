83101841e028929b2462ab402d7de21107b64c19
dahmri <dahmrik@gmail.com>
2019-04-30 15:59:16 +0200

    Delete Adamic_Adar.py

--- NetworkFeatures/Adamic_Adar.py
+++
@@ -1,9 +0,0 @@
-def adamic_adar_score(graph):
-    """Computes Adar-Adamic similarity matrix for an adjacency matrix"""
-
-    N = graph.vcount()
-    AA = np.zeros((N,N))
-
-    AA = graph.similarity_inverse_log_weighted(vertices=None, mode="ALL")
-
-    return np.array(AA)
