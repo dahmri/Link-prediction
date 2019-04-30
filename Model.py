93c103a9c729f9e70118667a36e2c8ad4f641a12
dahmri <dahmrik@gmail.com>
2019-04-30 16:40:24 +0200

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,20 +1,20 @@
+import findspark
+findspark.init()
 from pyspark import SparkContext, SparkConf
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.sql import SQLContext
 from pyspark.sql.types import *
 from pyspark.mllib.tree import RandomForest
-from pyspark.mllib.classification import SVMWithSGD, SVMModel ,LogisticRegressionWithSGD
+from pyspark.mllib.classification import SVMWithSGD, SVMModel, LogisticRegressionWithSGD
 from pyspark.mllib.util import MLUtils
-from pyspark.mllib.evaluation import BinaryClassificationMetrics,MulticlassMetrics
+from pyspark.mllib.evaluation import BinaryClassificationMetrics, MulticlassMetrics
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-import matplotlib.pyplot as plt
-from pylab import title,gcf
+
 
 
 def parsePoint(line):
     values = line.split(' ')
-    return LabeledPoint(values[7],values[0:7])
+    return LabeledPoint(values[7], values[0:7])
 
 
 conf = SparkConf().set('spark.driver.memory', '50G').set('spark.executor.memory', '8G').setMaster("local[4]")
@@ -22,18 +22,18 @@ sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 
 
-Data="DataFTrainTest/dtrain_Trades_Network.csv"
+Data="./TrainTest/dtrain_Trades_Network.csv"
 data1 = sc.textFile(Data)
 trainData = data1.map(parsePoint)
 
 trainData.persist()
 print ("We start training")
 #model = SVMWithSGD.train(trainData, iterations=1)
-model = LogisticRegressionWithSGD.train(trainData,1)
+model = LogisticRegressionWithSGD.train(trainData, 1)
 model.clearThreshold()
 
 
-Test="DataFTrainTest/dtest_Trades_Network.csv"
+Test="./TrainTest/dtest_Trades_Network.csv"
 data2 = sc.textFile(Test)
 testData = data2.map(parsePoint)
 
