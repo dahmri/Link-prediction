072bbf2e43eda1a8dde3560980adee69c317a602
dahmri <dahmrik@gmail.com>
2019-05-04 17:31:05 +0200

    Update CreatingFeatures.py

--- CreatingFeatures.py
+++ CreatingFeatures.py
@@ -35,7 +35,7 @@ def FeatureMatrix(graph, date):
     betweenness = NetworkFeatures.betweenness_centrality_score(graph)
 
     dailyRate = []
-    dailyRate = WeightMovingAverageTimeSeriesRate(graph, date)
+    dailyRate = NetworkFeatures.WeightMovingAverageTimeSeriesRate(graph, date)
 
 
     return AAMatrix, JCMatrix, CNMatrix, PAMatrix, SPMatrix, betweenness, dailyRate
@@ -56,59 +56,10 @@ def FeatureVector(nodei, nodej, AAMatrix, JCMatrix, CNMatrix, PAMatrix, SPMatrix
 
     return feature_vector
 
-def WeightMovingAverageTimeSeriesRate(graph,date):
-    """
-    Computing a daily Forecast from a graph using Weight Moving Average rate
-    """
-
-    move = 3
-    alpha = 0.2
-    beta = 0.3
-    gamma = 0.5
-
-    dailyRate = zeros(graph.vcount())
-    dailyForecast = zeros(graph.vcount())
-
-    baseDate = "1"
-    diff = int(date) - int(baseDate)
-    if diff > move:
-        baseDate = int(baseDate) + (diff-move-1)
-        baseDate = str(baseDate)
-
-    for v in range(graph.vcount()):
-        n = 0
-        new_date = int(baseDate) + n
-        while new_date <= int(date):
-            new_graph = Graph.Read_GraphML("Data/trades-timestamped-2009-12-"+str(new_date)+".graphml")
-            if (v < new_graph.vcount()):
-
-                index = (new_graph.vs[v]).index
-                if n == 0:
-                    dailyRate[v] = dailyRate[v] + alpha * new_graph.degree(index, mode='OUT')
-                if n == 1:
-                    dailyRate[v] = dailyRate[v] + beta * new_graph.degree(index, mode='OUT')
-                if n == 2:
-                    dailyRate[v] = dailyRate[v] + gamma * new_graph.degree(index, mode='OUT')
-
-            else:
-                dailyRate[v] = dailyRate[v] + 0
-
-            n += 1
-            new_date = int(baseDate) + n
-        dailyForecast[v] = dailyRate[v] / n
-    return dailyForecast
 
 
 
-
-
-
-
-
-
-
-
-for date in range(5, 30):
+for date in range(1, 30):
 
     DataGraph = Graph.Read_GraphML("Data/trades-timestamped-2009-12-"+str(date)+".graphml")
     print("trades-timestamped-2009-12-"+str(date)+".graphml")
